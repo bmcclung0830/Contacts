da9c1998f651d14bee52cecbdae9ccbc7fdfc6c9
Brittany McClung <Brittany@Brittanys-MacBook-Pro.local>
2016-04-12 15:18:03 -0500

    assignment

---
+++ src/sample/Controller.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    ObservableList<Contact> contacts = FXCollections.observableArrayList();
+
+    @FXML
+    ListView list;
+
+    @FXML
+    TextField name;
+
+    @FXML
+    TextField number;
+
+    @FXML
+    TextField email;
+
+    public void addContact(){
+        Contact newContact = new Contact( name.getText(), number.getText(), email.getText() );
+        contacts.add(newContact);
+    }
+    public void removeContact (){
+        Contact contact = (Contact) list.getSelectionModel().getSelectedItem();
+        contacts.remove(contact);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        list.setItems(contacts);
+    }
+}
